10dd9fa145f96fda8550935963f7de8cae993a9c
fundon <cfddream@gmail.com>
2013-11-14 10:52:00 +0800

    Update

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class squirrel {
   package { 'Squirrel':
     source   => 'http://dl.bintray.com/lotem/rime/Squirrel-0.9.22.zip?direct',
-    provider => 'compressed_pkg'
+    provider => 'compressed'
   }
 }
--- spec/classes/squirrel_spec.rb
+++ spec/classes/squirrel_spec.rb
@@ -4,7 +4,7 @@ describe 'squirrel' do
   it do
     should contain_package('Squirrel').with({
       :source   => 'http://dl.bintray.com/lotem/rime/Squirrel-0.9.22.zip?direct',
-      :provider => 'compressed_pkg'
+      :provider => 'compressed'
     })
   end
 end
